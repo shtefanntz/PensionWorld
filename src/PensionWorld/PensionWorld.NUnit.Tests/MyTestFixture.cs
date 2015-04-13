a395b751fe4725e76b269cddecc50803a71f10b1
mteletin <mircea.teletin@outlook.com>
2015-04-13 12:26:16 +0300

    random mock test fixture

---
+++ src/PensionWorld/PensionWorld.NUnit.Tests/MyTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using NUnit.Core;
+using NUnit.Framework;
+
+namespace PensionWorld.NUnit.Tests
+{
+    [TestFixture]
+    public class MyTestFixture
+    {
+        [Test]
+        public TestThatSomethingIsWorking()
+        {
+            //Arange - SuT (System under Test)
+
+            //Act
+
+            //Assert - NUnit specific assertions (can also be done from attributes)
+        }
+    }
+}
